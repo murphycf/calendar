2936b8f5e2c50af86414e12296faee85a94c9bd1
murphycf@pm.me <murphycf@pm.me>
2022-01-13 08:50:59 -0600

    html updates

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app =  Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello"
+
+@app.route('/calendar/')
+def calendar():
+    return render_template("calendar.html")
 
 if __name__ == "main":
     app.run(debug=True, port=5000)
