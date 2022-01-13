b963e03ae18259d3d2b4ccda3eeb182dc002f893
murphycf@pm.me <murphycf@pm.me>
2022-01-13 08:52:25 -0600

    a

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app =  Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello"
-
-@app.route('/calendar/')
 def calendar():
     return render_template("calendar.html")
 
